1ed1f689b0c7974ccdd63c6820f46eaf9a120c6e
tomconnat <connattom@gmail.com>
2021-11-24 18:08:16 +0100

    tomv1

---
+++ main.c
@@ -0,0 +1,17 @@
+//
+// Created by tomco on 24/11/2021.
+//
+
+#include <stdio.h>
+#include <winnt.h>
+#include "fonction.h"
+
+
+
+int main() {
+    //Menu();
+    //LancerDes();
+    //initialisationDesJoueur();
+    plateaudejeux();
+    return 0;
+}
